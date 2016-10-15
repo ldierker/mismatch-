c6622194a70f7beedec0732f03c3d498b5e40c1c
Daniel Navon <dnavon2557@gmail.com>
2016-10-14 23:44:21 -0400

    generated server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var validator = require('validator');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(__dirname));
+
+
+app.listen(process.env.PORT || 5000);
