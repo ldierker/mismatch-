98ffb31df9f06e9dc53afb649402882a14338d52
Daniel Navon <dnavon2557@gmail.com>
2016-10-15 00:44:23 -0400

    ajax still not working

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var validator = require('validator');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/colortag_testing"));
 
 
 app.listen(process.env.PORT || 5000);
